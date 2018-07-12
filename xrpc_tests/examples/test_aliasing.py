8eeda370c67b461443a7af91adf9ea42dc92d93d
Andrey Cizov <acizov@gmail.com>
2018-07-12 04:24:41 +0100

    Created

--- xrpc_tests/examples/test_aliasing.py
+++ xrpc_tests/examples/test_aliasing.py
@@ -1,5 +1,6 @@
 from _signal import SIGTERM
 
+from xrpc.client import ClientConfig
 from xrpc.dsl import regular, rpc, signal
 from xrpc_tests.examples.test_generic import build_ts
 from xrpc_tests.mp.abstract import ProcessHelperCase, server_main
@@ -30,7 +31,7 @@ class TestAliasing(ProcessHelperCase):
         url_b = 'udp://127.0.0.1:32456?recv_reply=message_2'
         a = self.ps.popen(server_main, aliasing_main, url_b)
 
-        with build_ts(BaseClass, url_b) as b:
+        with build_ts(BaseClass, url_b, ClientConfig(ignore_horizon=True)) as b:
             self.assertEqual(b.recv_reply(5), 6)
 
         a.send_signal(SIGTERM)
