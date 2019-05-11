8d882d7f06c6669340a2ce46e2a018d03f024c05
Andrey Cizov <acizov@gmail.com>
2019-05-11 15:10:15 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info >= (3, 7):
 
 setup(
     name='xrpc',
-    version='0.0.3',
+    version='0.0.4',
     author='Andrey Cizov',
     author_email='acizov@gmail.com',
     packages=find_packages(include=('xrpc', 'xrpc.*',)),
