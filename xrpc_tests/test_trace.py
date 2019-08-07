79f5eab81276209e9820ce3bc76240794f04d0ea
Andrey Cizov <acizov@gmail.com>
2019-08-07 12:38:10 +0100

    fix trace tests

--- xrpc_tests/test_trace.py
+++ xrpc_tests/test_trace.py
@@ -16,18 +16,18 @@ class TestTrace(unittest.TestCase):
     def test_trace(self):
         x = _trace()
 
-        self.assertEqual(f'{self.__module__}._trace', x.name)
+        self.assertEqual(f'{self.__module__}._trace:12', x.name)
 
     def test_trace_postfix(self):
         x = _trace('pf')
-        self.assertEqual(f'{self.__module__}._trace.pf', x.name)
+        self.assertEqual(f'{self.__module__}._trace:12.pf', x.name)
 
     def test_trace_obj(self):
         x = Traced().a()
 
-        self.assertEqual(f'{self.__module__}.{Traced.__name__}.a', x.name)
+        self.assertEqual(f'{self.__module__}.{Traced.__name__}.a:8', x.name)
 
     def test_trace_obj_postfix(self):
         x = Traced().a('pf')
 
-        self.assertEqual(f'{self.__module__}.{Traced.__name__}.a.pf', x.name)
+        self.assertEqual(f'{self.__module__}.{Traced.__name__}.a:8.pf', x.name)
